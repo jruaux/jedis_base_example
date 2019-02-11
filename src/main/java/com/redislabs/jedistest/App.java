5350e73b0d03f256e6aa02689047f96391e1fe39
Chris Mague <github@mague.com>
2019-02-11 15:15:28 -0800

    use a map

--- src/main/java/com/redislabs/jedistest/App.java
+++ src/main/java/com/redislabs/jedistest/App.java
@@ -1,6 +1,8 @@
 package com.redislabs.jedistest;
 
 import redis.clients.jedis.Jedis;
+import java.util.Map;
+
 
 public class App 
 {
@@ -9,7 +11,8 @@ public class App
         System.out.println( "Connecting to Redis!" );
         try {
             Jedis jedis = new Jedis("localhost"); 
-            System.out.println(jedis.hgetAll("56714964282381505346596299146714128775465616245895347306009810006048170613624066755")); 
+            Map<String, String> dataMap = jedis.hgetAll("56714964282381505346596299146714128775465616245895347306009810006048170613624066755"); 
+            System.out.println(dataMap);
         } catch (Exception e) {
             System.out.println("Unable to connect to Redis Server");
         }
